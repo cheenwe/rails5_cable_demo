8cef616f2fcf7aaa85edb94966356c4aa8478a86
cheenwe <cheenwe@gmail.com>
2015-12-23 17:39:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## README
+Rails 5 Demo of Action Cable https://www.youtube.com/watch?v=n0WUjGkDFS0&feature=youtu.be by DHH
+ 
---
+++ app/channels/room_channel.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "room_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak(data)
+    #存在时,创建数据
+    Message.create!(content: data['message']) if data['message'].present?
+  end
+end
